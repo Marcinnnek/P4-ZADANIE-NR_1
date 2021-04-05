caa535ae442535b876fe1d4ad028741e4a6ac0f2
Marcin Zuziak <mz048257@student.ath.edu.pl>
2021-04-05 18:49:34 +0200

    Przeniesienie logiki bazy danych do kalsy

---
+++ P4 ZADANIE/DBconnection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace P4_ZADANIE
+{
+    class DBconnection
+    {
+
+        private SqlConnection _dbConnection;
+        public DBconnection(string connectionString)
+        {
+            _dbConnection = new SqlConnection(connectionString);
+        }
+
+        public void SqlDeleteZN()
+        {
+            Console.WriteLine("Usunięcie wiersza.");
+            _dbConnection.Open();
+
+            var deleteSQL = "DELETE from mg.Kategorie WHERE IDkategorii = @IDkat";
+            var deleteCommand = new SqlCommand(deleteSQL, _dbConnection);
+
+            deleteCommand.Parameters.Add(new SqlParameter("IDkat", 15));
+            deleteCommand.ExecuteNonQuery();
+
+            _dbConnection.Close();
+        }
+
+       public void SqlUpdateZN()
+        {
+            Console.WriteLine("Aktualizacja wartości w danym wierszu.");
+            _dbConnection.Open();
+
+            var updateSQL = "UPDATE mg.Kategorie SET NazwaKategorii = @newName, Opis = @newDescription WHERE IDkategorii = @IDkat";
+            var updateCommand = new SqlCommand(updateSQL,  _dbConnection);
+
+            updateCommand.Parameters.Add(new SqlParameter("@newName", "AKT. nazwy kategorii"));
+            updateCommand.Parameters.Add(new SqlParameter("@newDescription", "AKT. Opis nowej kategorii"));
+            updateCommand.Parameters.Add(new SqlParameter("IDkat", 15));
+            updateCommand.ExecuteNonQuery();
+
+            _dbConnection.Close();
+        }
+
+        public void SqlInsertZN()
+        {
+            Console.WriteLine("Wstawienie nowego wiersza.");
+            _dbConnection.Open();
+
+            var insertSQL = "INSERT INTO mg.Kategorie (IDkategorii, NazwaKategorii, Opis) VALUES (@IDkat, @Nkat, @Op)";
+            var insertCommand = new SqlCommand(insertSQL, _dbConnection);
+
+            insertCommand.Parameters.Add(new SqlParameter("IDkat", 15));
+            insertCommand.Parameters.Add(new SqlParameter("Nkat", "Nowa Kategoria"));
+            insertCommand.Parameters.Add(new SqlParameter("Op", "Opis nowej kategorii"));
+            insertCommand.ExecuteNonQuery();
+
+            _dbConnection.Close();
+        }
+
+        public void SqlReaderZN()
+        {
+            Console.WriteLine("Odczytanie wierszy tabeli.");
+            _dbConnection.Open();
+            var readSQL = "SELECT * FROM mg.Kategorie";
+            var readCommand = new SqlCommand(readSQL, _dbConnection);
+
+            var reader = readCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.Write($"{reader.GetInt32(0)} - {reader.GetString(1)} -  {reader.GetString(2)}");
+                Console.WriteLine();
+            }
+
+            _dbConnection.Close();
+        }
+    }
+
+}
--- P4 ZADANIE/Program.cs
+++ P4 ZADANIE/Program.cs
@@ -7,83 +7,26 @@ namespace P4_ZADANIE
     {
         static void Main(string[] args)
         {
-            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ZNorthwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
+                                    Initial Catalog=ZNorthwind;
+                                    Integrated Security=True;
+                                    Connect Timeout=30;Encrypt=False;
+                                    TrustServerCertificate=False;
+                                    ApplicationIntent=ReadWrite;
+                                    MultiSubnetFailover=False";
 
-            SqlConnection connection = new SqlConnection(connectionString);
+            var dataBaseObj = new DBconnection(connectionString);
 
-            SqlReaderZN(connection);
+            dataBaseObj.SqlReaderZN();
             Console.WriteLine();
-            SqlInsertZN(connection);
-            SqlReaderZN(connection);
+            dataBaseObj.SqlInsertZN();
+            dataBaseObj.SqlReaderZN();
             Console.WriteLine();
-            SqlUpdateZN(connection);
-            SqlReaderZN(connection);
+            dataBaseObj.SqlUpdateZN();
+            dataBaseObj.SqlReaderZN();
             Console.WriteLine();
-            SqlDeleteZN(connection);
-            SqlReaderZN(connection);
-        }
-
-        private static void SqlDeleteZN(SqlConnection connection)
-        {
-            Console.WriteLine("Usunięcie wiersza.");
-            connection.Open();
-
-            var deleteSQL = "DELETE from mg.Kategorie WHERE IDkategorii = @IDkat";
-            var deleteCommand = new SqlCommand(deleteSQL, connection);
-
-            deleteCommand.Parameters.Add(new SqlParameter("IDkat", 15));
-            deleteCommand.ExecuteNonQuery();
-
-            connection.Close();
-        }
-
-        private static void SqlUpdateZN(SqlConnection connection)
-        {
-            Console.WriteLine("Aktualizacja wartości w danym wierszu.");
-            connection.Open();
-
-            var updateSQL = "UPDATE mg.Kategorie SET NazwaKategorii = @newName, Opis = @newDescription WHERE IDkategorii = @IDkat";
-            var updateCommand = new SqlCommand(updateSQL, connection);
-
-            updateCommand.Parameters.Add(new SqlParameter("@newName", "AKT. nazwy kategorii"));
-            updateCommand.Parameters.Add(new SqlParameter("@newDescription", "AKT. Opis nowej kategorii"));
-            updateCommand.Parameters.Add(new SqlParameter("IDkat", 15));
-            updateCommand.ExecuteNonQuery();
-
-            connection.Close();
-        }
-
-        private static void SqlInsertZN(SqlConnection connection)
-        {
-            Console.WriteLine("Wstawienie nowego wiersza.");
-            connection.Open();
-
-            var insertSQL = "INSERT INTO mg.Kategorie (IDkategorii, NazwaKategorii, Opis) VALUES (@IDkat, @Nkat, @Op)";
-            var insertCommand = new SqlCommand(insertSQL, connection);
-
-            insertCommand.Parameters.Add(new SqlParameter("IDkat", 15));
-            insertCommand.Parameters.Add(new SqlParameter("Nkat", "Nowa Kategoria"));
-            insertCommand.Parameters.Add(new SqlParameter("Op", "Opis nowej kategorii"));
-            insertCommand.ExecuteNonQuery();
-
-            connection.Close();
-        }
-
-        private static void SqlReaderZN(SqlConnection connection)
-        {
-            Console.WriteLine("Odczytanie wierszy tabeli.");
-            connection.Open();
-            var readSQL = "SELECT * FROM mg.Kategorie";
-            var readCommand = new SqlCommand(readSQL, connection);
-
-            var reader = readCommand.ExecuteReader();
-            while (reader.Read())
-            {
-                Console.Write($"{reader.GetInt32(0)} - {reader.GetString(1)} -  {reader.GetString(2)}");
-                Console.WriteLine();
-            }
-
-            connection.Close();
+            dataBaseObj.SqlDeleteZN();
+            dataBaseObj.SqlReaderZN();
         }
     }
 }
